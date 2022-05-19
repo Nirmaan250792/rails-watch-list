c3c581da678cd8f7a3cf43a92cbe71f7b22de89f
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-05-19 20:38:40 +0400

    PArtly done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/create'
-  get 'lists/show'
-  get 'lists/new'
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only: [:create, :index, :show, :new] do
-    resources :bookmarks, only: [:create, :new]
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
-  resources :bookmarks, only: [:destroy]
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Deleting DB"
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+# Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+Movie.destroy_all
+puts "creating movies"
+movie_1 = Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+movie_2 = Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+movie_3 = Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+movie_4 = Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "completed"
 
-puts "DB Created"
+puts "creating list"
+list_1 = List.create(name: "Action")
+list_2 = List.create(name:"Sci-Fi")
+list_3 = List.create(name:"Thriller")
+puts "list created"
+
+# puts "#{list_1.id}"
+# puts "#{movie_1.id}"
+
+puts "creating bookmarks"
+Bookmark.create(comment:"All times favorite", movie_id: "#{movie_1.id}", list_id: "#{list_1.id}")
+Bookmark.create(comment:"Need to watch", movie_id: "#{movie_2.id}", list_id: "#{list_3.id}" )
+Bookmark.create(comment:"Oldies", movie_id: "#{movie_3.id}", list_id: "#{list_1.id}")
+Bookmark.create(comment:"5 stars", movie_id: "#{movie_4.id}", list_id: "#{list_2.id}")
+puts "bookmarks created"
